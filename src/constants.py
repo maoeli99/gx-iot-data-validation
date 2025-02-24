d609bbb942f5f624b3769d28198b07e9bb229056
Ölmaier Martin <m.oelmaier@vollmer-group.com>
2025-02-24 18:43:23 +0100

    constants hinzugefügt

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+# Datenbank-Konstanten
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_NAME = "postgres"
+DB_TABLE = "measurement"
+
+# GX-Konstanten
+DATA_SOURCE = "postgres_weather_db"
+DATA_ASSET = "measurement"
+BATCH_DEFINITION = "last_day_batch"
+EXPECTATION_SUITE = "weather_data_expectation_suite"
+VALIDATION_DEFINITION = "weather_data_validation_definition"
+CHECKPOINT_NAME = "weather_data_validation_checkpoint"
+INTERVAL_SECONDS = 10
+CYCLE_TIME = 0.05
